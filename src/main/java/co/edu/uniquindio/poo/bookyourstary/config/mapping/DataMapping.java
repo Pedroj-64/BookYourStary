3fd6188d661c61e9821c8a0c393c7da547223b8f
Pedroj-64 <pedroj.sotor@uqvirtual.edu.co>
2025-05-22 17:12:58 -0500

    LOCOOO

--- src/main/java/co/edu/uniquindio/poo/bookyourstary/config/mapping/DataMapping.java
+++ src/main/java/co/edu/uniquindio/poo/bookyourstary/config/mapping/DataMapping.java
@@ -58,56 +58,78 @@ public class DataMapping {
             LinkedList<ServiceIncluded> serviciosBasicos = new LinkedList<>();
             serviciosBasicos.add(new ServiceIncluded("1", "Wifi", "Internet inalámbrico disponible"));
             serviciosBasicos.add(new ServiceIncluded("2", "TV", "Televisión por cable incluida"));
-            LinkedList<ServiceIncluded> serviciosPremium = new LinkedList<>();
-            serviciosPremium.add(new ServiceIncluded("1", "Wifi", "Internet inalámbrico disponible"));
-            serviciosPremium.add(new ServiceIncluded("2", "TV", "Televisión por cable incluida"));
-            serviciosPremium.add(new ServiceIncluded("3", "Desayuno", "Desayuno incluido todas las mañanas"));
 
-            // Ciudades de ejemplo
+            LinkedList<ServiceIncluded> serviciosPremium = new LinkedList<>();
+            serviciosPremium.addAll(serviciosBasicos);
+            serviciosPremium.add(new ServiceIncluded("3", "Piscina", "Acceso a piscina"));
+            serviciosPremium.add(new ServiceIncluded("4", "Gimnasio", "Acceso a gimnasio"));
+
+            // Crear ciudades de ejemplo
             City armenia = new City("Armenia", "Colombia", "Quindio");
             City cali = new City("Cali", "Colombia", "Valle");
             City bogota = new City("Bogotá", "Colombia", "Cundinamarca");
+            City medellin = new City("Medellín", "Colombia", "Antioquia");
 
-            // Habitaciones para hoteles
+            // Crear habitaciones para hoteles
             LinkedList<Room> roomsHotelSol = new LinkedList<>();
-            roomsHotelSol.add(new Room("101", 120000, 2, "FotoHotelRelleno.png", "Habitación doble"));
-            roomsHotelSol.add(new Room("102", 150000, 3, "FotoHotelRelleno.png", "Habitación triple"));
+            roomsHotelSol.add(new Room("101", 100000, 2, "FotoHotelRelleno.png", "Habitación doble"));
+            roomsHotelSol.add(new Room("102", 150000, 4, "FotoHotelRelleno.png", "Suite familiar"));
+
             LinkedList<Room> roomsHotelLuna = new LinkedList<>();
-            roomsHotelLuna.add(new Room("201", 100000, 2, "FotoHotelRelleno.png", "Habitación doble"));
-            roomsHotelLuna.add(new Room("202", 130000, 3, "FotoHotelRelleno.png", "Habitación triple"));
+            roomsHotelLuna.add(new Room("201", 80000, 1, "FotoHotelRelleno.png", "Habitación individual"));
+            roomsHotelLuna.add(new Room("202", 120000, 2, "FotoHotelRelleno.png", "Habitación doble premium"));
+
             LinkedList<Room> roomsHotelEstrella = new LinkedList<>();
             roomsHotelEstrella.add(new Room("301", 110000, 2, "FotoHotelRelleno.png", "Habitación doble"));
             roomsHotelEstrella.add(new Room("302", 140000, 3, "FotoHotelRelleno.png", "Habitación triple"));
 
-            // 3 Hoteles
+            // Crear hoteles
             MainController.getInstance().getHostingService().createHotel(
                     "Hotel Sol", armenia, "Hotel en Armenia", "sol.jpg", 120000, 5,
                     new LinkedList<>(serviciosPremium), new LinkedList<>(roomsHotelSol),
                     LocalDate.now(), LocalDate.now().plusDays(30));
+
             MainController.getInstance().getHostingService().createHotel(
                     "Hotel Luna", cali, "Hotel en Cali", "FotoHotelRelleno.png", 100000, 4,
                     new LinkedList<>(serviciosBasicos), new LinkedList<>(roomsHotelLuna),
                     LocalDate.now(), LocalDate.now().plusDays(30));
+
             MainController.getInstance().getHostingService().createHotel(
                     "Hotel Estrella", bogota, "Hotel en Bogotá", "FotoHotelRelleno.png", 110000, 6,
                     new LinkedList<>(serviciosPremium), new LinkedList<>(roomsHotelEstrella),
                     LocalDate.now(), LocalDate.now().plusDays(30));
 
-            // 3 Casas
+            // Crear casas
             MainController.getInstance().getHostingService().createHouse(
                     "Casa Verde", armenia, "Casa familiar", "FotoHotelRelleno.png",
                     200000, 4, new LinkedList<>(serviciosBasicos), 30000,
                     LocalDate.now(), LocalDate.now().plusDays(30));
+
             MainController.getInstance().getHostingService().createHouse(
                     "Casa Azul", cali, "Casa con piscina", "FotoHotelRelleno.png",
                     250000, 5, new LinkedList<>(serviciosPremium), 35000,
                     LocalDate.now(), LocalDate.now().plusDays(30));
+
             MainController.getInstance().getHostingService().createHouse(
                     "Casa Roja", bogota, "Casa céntrica", "FotoHotelRelleno.png",
                     180000, 6, new LinkedList<>(serviciosBasicos), 25000,
-                    LocalDate.now(), LocalDate.now().plusDays(30)); // No creamos apartamentos aquí - lo haremos desde
-                                                                    // App.java usando ApartmentCreator
-            System.out.println("La creación de apartamentos será manejada por ApartmentCreator");
+                    LocalDate.now(), LocalDate.now().plusDays(30));
+
+            // Crear apartamentos
+            MainController.getInstance().getAdminService().createApartament(
+                    "Apartamento Moderno", medellin, "Apartamento céntrico", "FotoApartamentoRelleno.png",
+                    150000, 3, new LinkedList<>(serviciosBasicos),
+                    LocalDate.now(), LocalDate.now().plusDays(30));
+
+            MainController.getInstance().getAdminService().createApartament(
+                    "Apartamento Familiar", bogota, "Apartamento espacioso", "FotoApartamentoRelleno.png",
+                    200000, 4, new LinkedList<>(serviciosPremium),
+                    LocalDate.now(), LocalDate.now().plusDays(30));
+
+            MainController.getInstance().getAdminService().createApartament(
+                    "Apartamento Vista", armenia, "Apartamento con vista", "FotoApartamentoRelleno.png",
+                    180000, 2, new LinkedList<>(serviciosBasicos),
+                    LocalDate.now(), LocalDate.now().plusDays(30));
 
             System.out.println("Creación de hostings de prueba completada con éxito.");
         } catch (Exception e) {
